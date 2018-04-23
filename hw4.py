8b378c88ff98229d38488d45a51b9fa2227d23f4
Rod Ta <rody101@gmail.com>
2018-04-23 18:28:55 -0400

    initial commit

---
+++ hw4.py
@@ -0,0 +1,22 @@
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+def group_restaurants():
+    spark = SparkSession(SparkContext())
+
+    df = spark.read.csv(path='/Users/RodTa/Desktop/spring_semester_2018/big_data/nyc_restaurants.csv',
+                        sep=',',
+                        encoding='UTF-8',
+                        comment=None,
+                        header=True,
+                        inferSchema=True)
+
+    table = df.groupBy('CUISINE DESCRIPTION').count().sort(['count'], ascending=False)
+
+    table.show(table.count())
+
+
+#group_restaurants()
+
+if __name__ == '__main__':
+    group_restaurants()

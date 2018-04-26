f74473ff8ea852ee83dc40affcbb37fc1f5cf63b
Rod Ta <rody101@gmail.com>
2018-04-26 15:48:22 -0400

    fixed path

--- hw4.py
+++ hw4.py
@@ -1,23 +1,20 @@
-import sys
+#import sys
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
 def group_restaurants():
     spark = SparkSession(SparkContext())
 
-    df = spark.read.csv(path='/Users/RodTa/Desktop/spring_semester_2018/big_data/nyc_restaurants.csv',
-                        sep=',',
-                        encoding='UTF-8',
-                        comment=None,
-                        header=True,
-                        inferSchema=True)
+    df = spark.read.csv('nyc_restaurants.csv',
+                      header=True,
+                      inferSchema=True)
 
     table = df.groupBy('CUISINE DESCRIPTION').count().sort(['count'], ascending=False)
 
     table.show(table.count())
 
 if __name__ == '__main__':
-    reload(sys)
-    sys.setdefaultencoding('utf8')
+    #reload(sys)
+    #sys.setdefaultencoding('utf8')
 
     group_restaurants()
